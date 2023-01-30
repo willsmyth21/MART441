2dd6d7245dba9f23a94beaab0c00f551c2d79376
Will Smyth <wsmyth2@gmail.com>
2023-01-29 21:25:05 -0700

    HW2commit

---
+++ HW2/script.js
@@ -0,0 +1,24 @@
+
+
+// my 3 favorite websites
+
+console.log('http://screenfull.net/stadium/2005_06_05_screenfull_archive.html')
+console.log('https://www.awwwards.com/awwwards/collections/404-error-page/')
+console.log('http://blog.animalswithinanimals.com/2008/08/databending-and-glitch-art-primer-part.html')
+
+
+// my 3 favorite games
+
+console.log("Papas Pizza Pileup");
+console.log("Bannanagrams");
+console.log("Fortnite");
+
+
+//my 3 favorite artists
+
+console.log("William Strobeck, an incredible filmmaker in New York City documenting modern skateboarding.");
+console.log("Walter Medcraft, a young and talented painter combining modern abstract skills to traditional famous paintings.");
+console.log("Babyxsosa, up and coming New York Rapper making groundbreaking music with new sounds and Jersey styled beats.");
+
+
+console.log("JavaScript Rules!");
